a8cfcf97129c629edeae7e805747e4446f31da8c
Hadia Akbar <hadiaa624@gmail.com>
2026-08-05 17:53:48 +0500

    Revert "Add files via upload"

--- ai-agents-feature-clean/src/controllers/analyticsController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { agentService } from '../services/agentService';
-import { asyncHandler } from '../middleware/errorHandler';
-
-export const getAnalytics = asyncHandler(async (req: Request, res: Response) => {
-  const clinicId = req.clinicId!;
-  const analytics = await agentService.getAnalytics(clinicId);
-  res.status(200).json(analytics);
-});
